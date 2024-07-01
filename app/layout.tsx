73e7056849e63e82dd360e5019a2fefab54c16b6
Amay Tripathi <amaytripathiwork@gmail.com>
2024-07-02 01:19:40 +0530

    Initial commit of my Airbnb clone project

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { Nunito } from "next/font/google";
+import getCurrentUser from "./actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
+
+//Modals
+import LoginModal from "./components/modals/LoginModal";
+import RegisterModal from "./components/modals/RegisterModal";
+import RentModal from "./components/modals/RentModal";
+
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import ToasterProvider from "./providers/ToasterProvider";
+import SearchModal from "./components/modals/SearchModal";
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone App ",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <LoginModal />
+          <RentModal />
+          <SearchModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
+      </body>
+    </html>
+  );
+}
